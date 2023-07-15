ac3bd3fbb7a9100799d8817473a54892036885f0
rdhwan <fathanridhwann@gmail.com>
2023-07-15 18:04:53 +0700

    refactor: refactoring organisator

--- user/controller/organisator.controller.js
+++ user/controller/organisator.controller.js
@@ -1,286 +1,428 @@
-const OrganisatorDB = require('../model/organisator.model')
-const StateDB = require('../../state/model/state_activities.model')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const { validateEmptyEntries } = require('../../helpers/FormValidator')
+const OrganisatorDB = require("../model/organisator.model");
+const StateDB = require("../../state/model/state_activities.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { validateEmptyEntries } = require("../../helpers/FormValidator");
+const {
+  registerValidator,
+  nimValidator,
+  verifyValidator,
+} = require("../validation/organisator.validation");
+const { loginValidator } = require("../validation/auth.validation");
 
 exports.registerOrganisator = async (req, res) => {
-
-    try {
-        const {name, nim, email, password, stateID} = req.body
-        const hashPass = await bcrypt.hashSync(password, 8)
-        const cekNIM = await OrganisatorDB.query().where({ nim })
-        const cekState = await StateDB.query().where({ stateID })
-
-        // Cek input kosong 
-        const emptyEntriesValidation = validateEmptyEntries(req.body);
-
-        if (emptyEntriesValidation.length > 0){
-            return res.status(400).send({
-                code : 400, 
-                message : "Kolom input kosong.",
-                error : emptyEntriesValidation
-            });
-        }
-
-        if(cekNIM.length !== 0){
-            return res.status(400).send({
-                message: 'Akun anda sebelumnya telah terdaftar'
-            })
-        }
-
-        // Misal kalo cuman boleh 1 yang register per state 
-        // if(cekState.length !== 0){
-        //     return res.status(400).send({
-        //         message: 'Sudah ada yang register menggunakan STATE tersebut!'
-        //     })
-        // }
-
-        if(cekState.length === 0 || cekState === []){
-            return res.status(409).send({ 
-                message: 'STATE yang kamu input tidak terdaftar!' 
-            })       
-        }
-
-        await OrganisatorDB.query().insert({
-            name,
-            nim,
-            password: hashPass,
-            email,
-            stateID, 
-            isverified: 0
-        })
-        return res.status(200).send({massage: 'Akun berhasil terdaftar!'})
+  const validateBody = await registerValidator.safeParseAsync(req.body);
+  if (!validateBody.success) {
+    return res.status(400).send({
+      code: 400,
+      message: "Validasi gagal.",
+      error: validateBody.error,
+    });
+  }
+  try {
+    const cekNIM = await OrganisatorDB.query()
+      .where({ nim: validateBody.data.nim })
+      .first();
+    if (cekNIM) {
+      return res.status(409).send({
+        code: 409,
+        message: "Akun anda sebelumnya telah terdaftar",
+      });
     }
-    catch (err) {
-        return res.status(500).send({ message: err.message })
+
+    const cekState = await StateDB.query()
+      .where({ stateID: validateBody.data.stateID })
+      .first();
+    if (!cekState) {
+      return res.status(409).send({
+        code: 409,
+        message: "STATE yang kamu input tidak terdaftar!",
+      });
     }
-}
+
+    validateBody.data.password = await bcrypt.hash(
+      validateBody.data.password,
+      10
+    );
+
+    // Misal kalo cuman boleh 1 yang register per state
+    // if(cekState.length !== 0){
+    //     return res.status(400).send({
+    //         message: 'Sudah ada yang register menggunakan STATE tersebut!'
+    //     })
+    // }
+
+    await OrganisatorDB.query().insert({
+      ...validateBody.data,
+      isverified: 0,
+    });
+    return res.status(200).send({
+      code: 200,
+      massage: "Akun berhasil terdaftar!",
+    });
+  } catch (err) {
+    return res.status(500).send({
+      code: 500,
+      message: err.message,
+    });
+  }
+};
 
 exports.loginOrganisator = async (req, res) => {
-    const {nim, password} = req.body
+  const validateBody = await loginValidator.safeParseAsync(req.body);
+  if (!validateBody.success) {
+    return res.status(400).send({
+      code: 400,
+      message: "Validasi gagal.",
+      error: validateBody.error,
+    });
+  }
 
-        // Cek input kosong 
-        const emptyEntriesValidation = validateEmptyEntries(req.body);
+  try {
+    const account = await OrganisatorDB.query()
+      .where({ nim: validateBody.data.nim })
+      .first();
 
-        if (emptyEntriesValidation.length > 0){
-            return res.status(400).send({
-                code : 400, 
-                message : "Kolom input kosong.",
-                error : emptyEntriesValidation
-            });
-        }
-
-    try {
-        const checkingnim = await OrganisatorDB.query().where({ nim })
-        //nim salah
-        if(checkingnim.length === 0){
-            return res.status(404).send({
-                message : 'NIM ' + nim + ' tidak terdaftar! Harap melakukan register!'
-            })
-        }
-        //password salah
-        const isPassValid = bcrypt.compareSync(password, checkingnim[0].password)
-        if(!isPassValid){
-            return res.status(400).send({
-                message: 'NIM atau password salah!'
-            })
-        }
-
-        const ver = await OrganisatorDB.query().select('isverified').where({ nim })
-        if(ver[0].isverified !== 1){
-            return res.status(400).send({
-                message: 'Akun anda belum terverifikasi!'
-            })
-        }
-        
-        const getStateName = await StateDB.query().where({ stateID: checkingnim[0].stateID })
-
-        const JWTtoken = jwt.sign({ 
-            name: checkingnim[0].name,
-            nim: checkingnim[0].nim,
-            email: checkingnim[0].email,
-            role: 'organisator',
-            stateName: checkingnim[0].name,
-            stateID: getStateName[0].stateID
-        },process.env.JWT_SECRET, {
-            expiresIn: 86400 //equals to 24H
-    })
-
-    return res.status(200).send({massage: 'Berhasil login!'
-    , token: JWTtoken
-})
-
+    //nim salah
+    if (!account) {
+      return res.status(404).send({
+        code: 404,
+        message: `NIM : ${validateBody.data.nim} tidak terdaftar! Harap melakukan register!`,
+      });
     }
-    catch (err) {
-        return res.status(500).send({ message: err.message })
+
+    //password salah
+    const isPassValid = await bcrypt.compare(
+      validateBody.data.password,
+      account.password
+    );
+    if (!isPassValid) {
+      return res.status(400).send({
+        code: 400,
+        message: "Password anda salah!",
+      });
     }
-}
+
+    if (!account.isverified) {
+      return res.status(400).send({
+        code: 400,
+        message: "Akun anda belum terverifikasi!",
+      });
+    }
+
+    const JWTtoken = jwt.sign(
+      {
+        nim: account.nim,
+        role: "organisator",
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: 86400, //equals to 24H
+      }
+    );
+
+    return res.status(200).send({
+      code: 200,
+      message: "Berhasil login!",
+      data: { token: JWTtoken, expiresIn: 86400 },
+    });
+  } catch (err) {
+    return res.status(500).send({
+      code: 500,
+      message: err.message,
+    });
+  }
+};
+
+exports.getProfile = async (req, res) => {
+  try {
+    const result = await OrganisatorDB.query()
+      .where({ nim: req.decoded_nim })
+      .first()
+      .join(
+        "state_activities",
+        "organisator.stateID",
+        "=",
+        "state_activities.stateID"
+      )
+      .select(
+        "organisator.nim",
+        "organisator.name",
+        "organisator.email",
+        "organisator.stateID",
+        "organisator.isverified",
+        "state_activities.name as stateName"
+      );
+
+    return res.status(200).send({
+      code: 200,
+      message: "Berhasil mendapatkan data profile",
+      data: result,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      code: 500,
+      message: err.message,
+    });
+  }
+};
 
 exports.getOrganisator = async (req, res) => {
-    try {
-        const result = await OrganisatorDB.query()
+  try {
+    const result = await OrganisatorDB.query()
+      .join(
+        "state_activities",
+        "organisator.stateID",
+        "=",
+        "state_activities.stateID"
+      )
+      .select(
+        "organisator.nim",
+        "organisator.name",
+        "organisator.email",
+        "organisator.stateID",
+        "organisator.isverified",
+        "state_activities.name as stateName"
+      );
 
-        for(let i = 0; i < result.length; i++){
-            const StateName = await StateDB.query().select('name')
-            .where({ stateID: result[i].stateID })
-
-            result[i].stateName = StateName[0].name
-        }
-
-        return res.status(200).send(result)    
-    } 
-    catch (err) {
-        return res.status(500).send({ message: err.message })
-    }
-}
+    return res.status(200).send({
+      code: 200,
+      message: "Berhasil mendapatkan data organisator",
+      data: result,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      code: 500,
+      message: err.message,
+    });
+  }
+};
 
 exports.getOrganisatorspesifik = async (req, res) => {
-    try {
-        const { nim } = req.params
+  const validateNim = await nimValidator.safeParseAsync(req.params.nim);
 
-        if(nim === null || nim === ':nim'){
-            return res.status(200).send({
-                message: 'NIM anda kosong! Harap diisi'
-            })
-        }
+  if (!validateNim.success) {
+    return res.status(400).send({
+      code: 400,
+      message: "Validasi gagal.",
+      error: validateNim.error,
+    });
+  }
 
-        const cekNIM = await OrganisatorDB.query().where({ nim })
-        if(cekNIM.length === 0 || cekNIM === []){
-            return res.status(200).send({ 
-                message: 'NIM ' + nim + ' tidak ditemukan'
-            }) 
-        }
-            
-        const result = await OrganisatorDB.query().where({ nim })
-        const Statename = await StateDB.query().select('name').where({ stateID: result[0].stateID })
-        result[0].Statenameisi = Statename[0].name
+  try {
+    const account = await OrganisatorDB.query()
+      .where({ nim: validateNim.data })
+      .first()
+      .join(
+        "state_activities",
+        "organisator.stateID",
+        "=",
+        "state_activities.stateID"
+      )
+      .select(
+        "organisator.nim",
+        "organisator.name",
+        "organisator.email",
+        "organisator.stateID",
+        "organisator.isverified",
+        "state_activities.name as stateName"
+      );
 
-        return res.status(200).send(result)
+    if (!account) {
+      return res.status(404).send({
+        code: 404,
+        message: `NIM : ${validateNim.data} tidak ditemukan`,
+      });
     }
-    catch (err) {
-        return res.status(500).send({ message: err.message })
+
+    return res.status(200).send({
+      code: 200,
+      message: "Berhasil mendapatkan data organisator",
+      data: account,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      code: 500,
+      message: err.message,
+    });
+  }
+};
+
+exports.updateOrganisator = async (req, res) => {
+  const validateNim = nimValidator.safeParse(req.params.nim);
+  if (!validateNim.success) {
+    return res.status(400).send({
+      code: 400,
+      message: "Validasi gagal.",
+      error: validateNim.error,
+    });
+  }
+
+  const validateBody = await registerValidator
+    .partial()
+    .safeParseAsync(req.body);
+  if (!validateBody.success) {
+    return res.status(400).send({
+      code: 400,
+      message: "Validasi gagal.",
+      error: validateBody.error,
+    });
+  }
+
+  try {
+    const authorizedDiv = ["D01", "D02"];
+    const division = req.divisiID;
+
+    if (!authorizedDiv.includes(division)) {
+      return res.status(403).send({
+        code: 403,
+        message: "Divisi anda tidak punya otoritas yang cukup!",
+      });
     }
-}
 
-exports.getUpdate = async (req, res) => {
-    try{
-        const { nim } = req.params
-        const { name, email, stateID } = req.body
-
-        if(nim === null || nim === ':nim'){
-            return res.status(404).send({
-                message: 'NIM anda kosong! Harap diisi'
-            })
-        }
-
-        const authorizedDiv = ['D01', 'D02']
-        const division = req.divisiID
-
-        if(!authorizedDiv.includes(division)){
-            return res.status(403).send({
-                message: "Divisi anda tidak punya otoritas yang cukup!"
-            })
-        }
-
-        const cekNIM = await OrganisatorDB.query().where({ nim })
-        if(cekNIM.length === 0 || cekNIM === []){
-            return res.status(404).send({ 
-                message: 'NIM ' + nim + ' tidak ditemukan!'
-            })
-        }
-
-        const liatState = await StateDB.query().select('stateID').where({ stateID: cekNIM[0].stateID })
-        if(liatState.length === 0 || liatState === []){
-            return res.status(403).send({
-                message: "State anda tidak terdaftar!"
-            })
-        }
-        await OrganisatorDB.query().update({
-            name,
-            email,
-            stateID
-        }).where({ nim })
-        return res.status(200).send({ message: 'Data berhasil diupdate' })
-        
-    }catch (err) {
-        return res.status(500).send({ message: err.message })
+    const account = await OrganisatorDB.query()
+      .where({ nim: validateNim.data })
+      .first();
+    if (!account) {
+      return res.status(404).send({
+        code: 404,
+        message: `NIM : ${validateNim.data} tidak ditemukan!`,
+      });
     }
-}
 
-
-exports.updateVerified = async(req, res) => {
-    try{
-        const { nim } = req.params
-
-        if(nim === null || nim === ':nim'){
-            return res.status(404).send({
-                message: 'NIM anda kosong! Harap diisi terlebih dahulu'
-            })
-        }
-
-        const { verified } = req.body
-        const authorizedDiv = ['D01', 'D02', 'D05']
-        const division = req.divisiID
-
-        if(!authorizedDiv.includes(division)){
-            return res.status(403).send({
-                message: "Divisi anda tidak punya otoritas yang cukup!"
-            })
-        }
-
-        const cekNIM = await OrganisatorDB.query().where({ nim })
-        if(cekNIM.length === 0 || cekNIM === []){
-            return res.status(404).send({ 
-                message: 'NIM ' + nim + ' tidak ditemukan!'
-            })
-        }
-
-        if(verified < 0 || verified > 1){
-            return res.status(406).send({ 
-                message: 'Value hanya boleh angka 0 atau 1 saja!' 
-            })
-        }
-
-        await OrganisatorDB.query().update({
-            verified
-        }).where({ nim })
-        return res.status(200).send({ message: 'Data berhasil diupdate' })
+    const liatState = await StateDB.query()
+      .select("stateID")
+      .where({ stateID: account.stateID })
+      .first();
+    if (!liatState) {
+      return res.status(403).send({
+        code: 403,
+        message: "State anda tidak terdaftar!",
+      });
     }
-    catch (err) {
-        return res.status(500).send({ message: err.message })
+
+    if (validateBody.data.password) {
+      validateBody.data.password = await bcrypt.hash(
+        validateBody.data.password,
+        10
+      );
     }
-}
+
+    await OrganisatorDB.query()
+      .where({ nim: validateNim.data })
+      .update(validateBody.data);
+
+    return res.status(200).send({
+      code: 200,
+      message: "Data berhasil diupdate",
+    });
+  } catch (err) {
+    return res.status(500).send({
+      code: 500,
+      message: err.message,
+    });
+  }
+};
+
+exports.verifyAcc = async (req, res) => {
+  const validateNim = await nimValidator.safeParseAsync(req.params.nim);
+
+  if (!validateNim.success) {
+    return res.status(400).send({
+      code: 400,
+      message: "Validasi gagal.",
+      error: validateNim.error,
+    });
+  }
+
+  const validateBody = await verifyValidator.safeParseAsync(req.body);
+  if (!validateBody.success) {
+    return res.status(400).send({
+      code: 400,
+      message: "Validasi gagal.",
+      error: validateBody.error,
+    });
+  }
+
+  try {
+    const authorizedDiv = ["D01", "D02", "D05"];
+    const division = req.divisiID;
+
+    if (!authorizedDiv.includes(division)) {
+      return res.status(403).send({
+        code: 403,
+        message: "Divisi anda tidak punya otoritas yang cukup!",
+      });
+    }
+
+    const cekNIM = await OrganisatorDB.query()
+      .where({ nim: validateNim.data })
+      .first();
+    if (!cekNIM) {
+      return res.status(404).send({
+        code: 404,
+        message: `NIM :${validateNim.data} tidak ditemukan!`,
+      });
+    }
+
+    await OrganisatorDB.query()
+      .update({
+        isverified: validateBody.data.isverified,
+      })
+      .where({ nim: validateNim.data });
+    return res.status(200).send({
+      code: 200,
+      message: "Data berhasil diupdate",
+    });
+  } catch (err) {
+    return res.status(500).send({
+      code: 500,
+      message: err.message,
+    });
+  }
+};
 
 exports.getDelete = async (req, res) => {
-    try {
-        const { nim } = req.params
-        const authorizedDiv = ['D01', 'D02']
-        const division = req.divisiID
+  const validateNim = await nimValidator.safeParseAsync(req.params.nim);
 
-        if(nim === null || nim === ':nim'){
-            return res.status(404).send({
-                message: 'NIM anda kosong! Harap diisi'
-            })
-        }
+  if (!validateNim.success) {
+    return res.status(400).send({
+      code: 400,
+      message: "Validasi gagal.",
+      error: validateNim.error,
+    });
+  }
 
-        if(!authorizedDiv.includes(division)){
-            return res.status(403).send({
-                message: "Divisi anda tidak punya otoritas yang cukup!"
-            })
-        }
+  try {
+    const authorizedDiv = ["D01", "D02"];
+    const division = req.divisiID;
 
-        const cekNIM = await OrganisatorDB.query().where({ nim })
-        if(cekNIM.length === 0 || cekNIM === []){
-            return res.status(404).send({ 
-                message: 'NIM ' + nim + ' tidak ditemukan!'
-            })
-        }
-    
-        await OrganisatorDB.query().delete().where({ nim })
-        return res.status(200).send({ message: 'Data berhasil dihapus' })
+    if (!authorizedDiv.includes(division)) {
+      return res.status(403).send({
+        code: 403,
+        message: "Divisi anda tidak punya otoritas yang cukup!",
+      });
     }
-    catch (err) {
-        return res.status(500).send({ message: err.message })
+
+    const cekNIM = await OrganisatorDB.query()
+      .where({ nim: validateNim.data })
+      .first();
+    if (!cekNIM) {
+      return res.status(404).send({
+        code: 404,
+        message: `NIM : ${validateNim.data} tidak ditemukan!`,
+      });
     }
-}
+
+    await OrganisatorDB.query().delete().where({ nim: validateNim.data });
+    return res.status(200).send({
+      code: 200,
+      message: "Data berhasil dihapus",
+    });
+  } catch (err) {
+    return res.status(500).send({
+      code: 500,
+      message: err.message,
+    });
+  }
+};
