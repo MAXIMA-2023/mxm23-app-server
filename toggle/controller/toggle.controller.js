005eb33510f5236d0f342916f9305348a7d76d7a
Gilbert H. Munayang <101878909+GIThuberHenry@users.noreply.github.com>
2023-07-16 21:03:40 +0700

    fix(toggle): typo variable and change authorized division

--- toggle/controller/toggle.controller.js
+++ toggle/controller/toggle.controller.js
@@ -16,18 +16,20 @@ exports.updateToggleValue = async(req,res)=>{
         const { id } = req.params
         const { toggle } = req.body
 
-        const authorizedDiv = ['D01', 'D02', 'D03', 'D04']
-        const division = req.division
+        const authorizedDiv = ['D01', 'D02', 'D05']
+        const division = req.divisiID
         const cekToggleID = await toggleDB.query().where({ id })
         
         if(!authorizedDiv.includes(division)){
             return res.status(403).send({
+                code: 403,
                 message: "Divisi anda tidak punya otoritas yang cukup!"
             })
         }
 
         if(cekToggleID.length === 0 || cekToggleID === []){
             return res.status(404).send({
+                code: 404,
                 message: "Endpoint pada toggle ID: " + id + " tidak ditemukan!"
             })
         }
@@ -36,7 +38,10 @@ exports.updateToggleValue = async(req,res)=>{
             return res.status(406).send({ message: 'Input value hanya antara 0 atau 1!' })
 
         await toggleDB.query().update({ toggle }).where({ id })
-        return res.status(200).send({ message: 'Data Toggle berhasil di update' })
+        return res.status(200).send({ 
+            code: 200,
+            message: 'Data Toggle berhasil di update' 
+        })
     }
     catch (err) {
         return res.status(500).send({ message: err.message })
