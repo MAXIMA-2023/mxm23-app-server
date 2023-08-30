757bb94cd9ff202d3378186c145a6ea58be801a5
EdwardLeonardoKosasih <edward.leonardo@student.umn.ac.id>
2023-08-30 21:33:54 +0700

    fix : database external table primary key

--- db_maxima23.sql
+++ db_maxima23.sql
@@ -5,6 +5,12 @@
 -- add isAttendedMalpun untuk maba dan external
 -- add ticketClaimed untuk maba
 -- add ticketBuyed untuk external
+
+-- DATABASE dev versi 25 agus 2023
+-- UPDATE:
+-- fix external table primary to transactionID
+-- added dummy transaction table
+
 CREATE TABLE `divisi` (
   `divisiID` char(3) NOT NULL, 
   `name` varchar(32) NOT NULL, 
@@ -43,8 +49,8 @@ CREATE TABLE `mahasiswa` (
   `prodi` varchar(32) NOT NULL,
   `token` varchar(32) NOT NULL,
   `created_at` datetime DEFAULT NOW(),
-  `alfagiftID` varchar(16) DEFAULT NULL,
-  `ticketClaimed` tinyint(1) DEFAULT NULL,
+  `alfagiftID` varchar(16) DEFAULT 0,
+  `ticketClaimed` tinyint(1) DEFAULT 0,
   `isAttendedMalpun` tinyint DEFAULT 0,
   PRIMARY KEY (`nim`) 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -54,19 +60,30 @@ INSERT INTO `mahasiswa` (`name`, `nim`, `password`, `whatsapp`, `email`, `angkat
 ('henry', 66484, '$2a$10$7XBGYMXUE6vsYUv/ohsgr.QrKR1efpEyQ9BnEhJBrrzjg.iorAgIy', '0852', 'gilbert.henry@student.umn.ac.id', 2023,'lineidgilbert', 'Infor', 'MXM23-66484');
 
 -- --------------------------------------------------------
+CREATE TABLE `dummytransaction`(
+  `transactionID` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY(`transactionID`)
+)Engine=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `dummytransaction` (`transactionID`) VALUES
+(1);
 
 CREATE TABLE `external` (
+  `transactionID` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
   `whatsapp` varchar(15) NOT NULL,
   `email` varchar(255) NOT NULL,
   `token` varchar(32) NOT NULL,
   `created_at` datetime DEFAULT NOW(),
-  `alfagiftID` varchar(16) DEFAULT NULL,
-  `ticketBuyed` tinyint(1) DEFAULT NULL,
-  `isAttendedMalpun` tinyint DEFAULT 0,
-  PRIMARY KEY (`nim`) 
+  `alfagiftID` varchar(16) DEFAULT 0,
+  `ticketBuyed` tinyint(1) DEFAULT 0,
+  `isAttendedMalpun` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`transactionID`),
+  FOREIGN KEY (`transactionID`) REFERENCES dummytransaction(`transactionID`) ON DELETE CASCADE ON UPDATE CASCADE 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `external`( `transactionID`, `name`, `whatsapp`, `email`, `token`) VALUES
+(1, 'Dummy External', '080809090707', 'external@gmail.com', 'MXM23-1');
 
 
 -- ------------------------------------------------------------
