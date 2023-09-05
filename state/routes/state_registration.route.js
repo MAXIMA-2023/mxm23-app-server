1a86fe2a0a23846c7605b59ad58d3fb3b78015a6
Gilbert H. Munayang <101878909+GIThuberHenry@users.noreply.github.com>
2023-09-05 20:03:20 +0700

    chore(state reg): fix typo import middleware

--- state/routes/state_registration.route.js
+++ state/routes/state_registration.route.js
@@ -62,14 +62,14 @@ module.exports = function (app) {
   app.post(
     "/api/state/attendance/first",
     middleware.verifyJWT,
-    middleware.isOrganisatorAndPanitiaInspice,
+    state_middleware.isOrganisatorAndPanitiaInspice,
     stateRegController.handleFirstAttendance
   );
 
   app.post(
     "/api/state/attendance/last",
     middleware.verifyJWT,
-    middleware.isOrganisatorAndPanitiaInspice,
+    state_middleware.isOrganisatorAndPanitiaInspice,
     stateRegController.handleLastAttendance
   );
 
