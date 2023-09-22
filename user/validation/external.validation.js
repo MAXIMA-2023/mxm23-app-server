29ae5ff4cfc57acc745b88480b8bf525b4cba27b
farrel dinarta <farrel.dinarta@student.umn.ac.id>
2023-09-22 21:13:02 +0700

    chore : edit whatsapp number requiremenet

--- user/validation/external.validation.js
+++ user/validation/external.validation.js
@@ -5,7 +5,10 @@ const registerValidator = z.object({
   email: z
     .string()
     .email("Format email tidak valid"),
-  whatsapp: z.string(),    
+  whatsapp: z
+    .string()
+    .min(10, "Nomor Whatsapp harus diatas 10 digit")
+    .max(15, "Nomor Whatsapp harus dibawah 15 digit"),   
 });
 
 module.exports = {
