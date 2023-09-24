51e8440ae562892bf21ee6c9867a3d4294378f33
Gilbert H. Munayang <101878909+GIThuberHenry@users.noreply.github.com>
2023-09-25 01:07:24 +0700

    chore(knex config): add log but commented

--- config/knexfile.js
+++ config/knexfile.js
@@ -13,8 +13,34 @@ module.exports = {
       password: process.env.DB_PASS || '', 
       database: process.env.DB_NAME || 'db_maxima23'
     },
-    debug: true
+    debug: true,
+    // log: {
+    //   debug: (message) =>
+    //     console.log(`[${new Date().toISOString()} - QUERY]: ${JSON.stringify(message)}`),
+    //   warn: (message) =>
+    //     console.error(`[${new Date().toISOString()} - WARN]: ${JSON.stringify(message)}`),
+    //   error: (message) =>
+    //     console.error(`[${new Date().toISOString()} - ERROR]: ${JSON.stringify(message)}`),
+    // },
   },
+  production: {
+    client: process.env.DB_CLIENT || 'mysql' ,
+    connection: {
+      host: process.env.DB_HOST || '127.0.0.1',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS || '', 
+      database: process.env.DB_NAME || 'db_maxima23'
+    },
+    debug: true,
+    // log: {
+    //   debug: (message) =>
+    //     console.log(`[${new Date().toISOString()} - QUERY]: ${message}`),
+    //   warn: (message) =>
+    //     console.error(`[${new Date().toISOString()} - WARN]: ${message}`),
+    //   error: (message) =>
+    //     console.error(`[${new Date().toISOString()} - ERROR]: ${message}`),
+    // },
+  }
 
   // staging: {
   //   client: 'postgresql',
