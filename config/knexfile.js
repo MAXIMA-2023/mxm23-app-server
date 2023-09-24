7f052b00fb6a851a5a9bd5a6fc42a22f3343f8ae
Gilbert H. Munayang <101878909+GIThuberHenry@users.noreply.github.com>
2023-09-24 23:15:36 +0700

    feat(knex config): add production

--- config/knexfile.js
+++ config/knexfile.js
@@ -15,6 +15,16 @@ module.exports = {
     },
     debug: true
   },
+  production: {
+    client: process.env.DB_CLIENT || 'mysql' ,
+    connection: {
+      host: process.env.DB_HOST || '127.0.0.1',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS || '', 
+      database: process.env.DB_NAME || 'db_maxima23'
+    },
+    debug: true
+  }
 
   // staging: {
   //   client: 'postgresql',
