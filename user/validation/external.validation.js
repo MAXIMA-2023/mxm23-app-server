0f0dba159f3ac5d52d9bc7b1248ff7a117845c76
rdhwan <fathanridhwan@gmail.com>
2023-10-05 17:49:22 +0700

    fix: make alfagift optional on external

--- user/validation/external.validation.js
+++ user/validation/external.validation.js
@@ -2,19 +2,18 @@ const z = require("zod");
 
 const registerValidator = z.object({
   name: z.string(),
-  email: z
-    .string()
-    .email("Format email tidak valid"),
+  email: z.string().email("Format email tidak valid"),
   whatsapp: z
     .string()
     .min(10, "Nomor Whatsapp harus diatas 10 digit")
-    .max(15, "Nomor Whatsapp harus dibawah 15 digit"),   
-  alfagiftID : z
+    .max(15, "Nomor Whatsapp harus dibawah 15 digit"),
+  alfagiftID: z
     .string()
-    .startsWith('99900')
-    .length(16, 'ID Alfagift tidak valid.')
+    .startsWith("99900")
+    .length(16, "ID Alfagift tidak valid.")
+    .optional(),
 });
 
 module.exports = {
-    registerValidator
-}
+  registerValidator,
+};
